3fe35b3da17fa306e06b1e447ce92ab3a4296e4c
kelmes1 <kyranovisch@gmail.com>
2025-02-14 18:35:53 +0100

    remove un needed files

--- old/data_tab.py
+++
@@ -1,5 +0,0 @@
-import pygame
-
-def draw_data_tab(screen):
-    # Dummy function for drawing data tab
-    pass
--- old/inv_tab.py
+++
@@ -1,7 +0,0 @@
-import pygame
-from global_functs import bottom_tab_lines2
-
-def draw_inv_tab(screen):
-    # Dummy function for drawing inventory tab
-    bottom_tab_lines2(screen)
-
